3e5f5b6f92c1d253c0cf4a6b6576358ab69e4156
Gene Andrei Manacop <218072162+GAMANACOP@users.noreply.github.com>
2025-07-15 09:07:06 +0800

    Update Group.cpp

--- Group.cpp
+++ Group.cpp
@@ -1,6 +1,8 @@
 #include "Group.h"
 #include <iostream>
 #include <string>
+#include <list>
+#include <deque>
 
 float DEFAULT_CHILD_DISCOUNT = 0.05;
 float DEFAULT_ADULT_DISCOUNT = 0.0;
@@ -8,6 +10,39 @@ float DEFAULT_SENIOR_DISCOUNT = 0.2;
 
 using namespace std;
 
+
+Group Table::getCurrentGroup() const {
+	return group;
+}
+
+int Table::getTableNumber() const {
+	return tableNumber;
+}
+
+void Table::setTableNumber (int num) {
+	tableNumber = num;
+}
+		
+void Table::setTableGroup(Group newGroup) {
+	group = newGroup;
+	hasGroup = true;
+}
+		
+bool Table::isTableEmpty() const {
+	return !hasGroup;
+}
+
+bool TablesList::hasAvailableTable() {
+	list<Table>::iterator iter;
+	
+	for (iter = tableList.begin(); iter != tableList.end(); iter++) {
+		if ((*iter).isTableEmpty()) {
+			return true;
+		}
+	}
+	
+	return false;
+}
 
 void Group::appendNode(Member newMember) {
 	MemberNode *newNode = new MemberNode;
